a4f16a1956448f1cfb8dbecc2a9da88167b73f87
Albertino Neto <albeneto72@gmail.com>
2021-03-05 18:46:25 -0300

    Projeto Alurapic em Vue Js

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
+    
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+
+        <meu-painel :titulo = "foto.titulo">
+            <imagem-responsiva :url="foto.url" :titulo="foto.titulo"></imagem-responsiva>
+        </meu-painel>
+
+      </li>
+    </ul>
+  </div>   
+  
+</template>
+
+<script>
+  import Painel from './components/shared/painel/Painel.vue';
+  import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
+  export default {
+    components: {
+      'meu-painel': Painel,
+      'imagem-responsiva': ImagemResponsiva
+    },
+    data(){
+      return {
+        titulo: 'Alurapic',
+        fotos: [],
+        filtro: ''
+      }
+    },
+    created(){
+      this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+    },
+    computed: {
+      fotosComFiltro(){
+        if(this.filtro){
+          // criando uma expressão com o valor do filtro, insensitivo
+          let exp = new RegExp(this.filtro.trim(), 'i');
+          // retorna apenas as fotos que condizem com a expressão
+          return this.fotos.filter(foto => exp.test(foto.titulo));
+        } else {
+          return this.fotos;
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  .centralizado{
+    text-align: center;
+  }
+
+  .corpo{
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  /* código anterior omitido */
+  .filtro {
+    display: block;
+    width: 100%;
+  }
+
+</style>
