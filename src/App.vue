2f5fc58e17d778a93ee6a9f42f2b94363e9978a4
Albertino Neto <albeneto72@gmail.com>
2021-03-07 20:25:36 -0300

    Estrutura SPA, VueRouter, Validação de propriedades, ternário para tratar a rota '' passanso '/'.

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,43 @@
 <template>
   <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
 
-    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
-    
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+    <meu-menu :rotas="routes"/>
 
-        <meu-painel :titulo = "foto.titulo">
-            <imagem-responsiva :url="foto.url" :titulo="foto.titulo"></imagem-responsiva>
-        </meu-painel>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
 
-      </li>
-    </ul>
   </div>   
-  
 </template>
 
 <script>
-  import Painel from './components/shared/painel/Painel.vue';
-  import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
+  import { routes } from './routes';
+  import Menu from './components/shared/menu/Menu.vue';
   export default {
     components: {
-      'meu-painel': Painel,
-      'imagem-responsiva': ImagemResponsiva
+      'meu-menu': Menu
     },
     data(){
       return {
-        titulo: 'Alurapic',
-        fotos: [],
-        filtro: ''
-      }
-    },
-    created(){
-      this.$http.get('http://localhost:3000/v1/fotos')
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
-    },
-    computed: {
-      fotosComFiltro(){
-        if(this.filtro){
-          // criando uma expressão com o valor do filtro, insensitivo
-          let exp = new RegExp(this.filtro.trim(), 'i');
-          // retorna apenas as fotos que condizem com a expressão
-          return this.fotos.filter(foto => exp.test(foto.titulo));
-        } else {
-          return this.fotos;
-        }
+        routes
       }
     }
   }
 </script>
 
 <style>
-  .centralizado{
-    text-align: center;
-  }
-
   .corpo{
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
     width: 96%;
   }
 
-  .lista-fotos {
-    list-style: none;
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0
   }
 
-  .lista-fotos .lista-fotos-item {
-    display: inline-block;
-  }
-
-  /* código anterior omitido */
-  .filtro {
-    display: block;
-    width: 100%;
+  .pagina-active, .pagina-leave-active {
+    transition: opacity .2s
   }
 
 </style>
